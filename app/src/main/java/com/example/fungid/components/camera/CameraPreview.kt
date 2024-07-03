752899b44b2c011ed075e58ae7fa10855c8856f4
Tomșa Alexandru-Eduard <tomsaalex@gmail.com>
2024-07-03 19:04:12 +0300

    Remade the camera UI. Now the Android View has the same aspect ratio as the camera input, which gets rid of the black bars. The new design should also be more responsive.

--- app/src/main/java/com/example/fungid/components/camera/CameraPreview.kt
+++ app/src/main/java/com/example/fungid/components/camera/CameraPreview.kt
@@ -2,7 +2,10 @@ package com.example.fungid.components.camera
 
 import android.view.ViewGroup
 import androidx.camera.core.Preview
+import androidx.camera.core.resolutionselector.AspectRatioStrategy
+import androidx.camera.core.resolutionselector.ResolutionSelector
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
@@ -10,11 +13,11 @@ import androidx.compose.ui.viewinterop.AndroidView
 @Composable
 fun CameraPreview(
     modifier: Modifier = Modifier,
-    scaleType: PreviewView.ScaleType = PreviewView.ScaleType.FIT_START,
+    scaleType: PreviewView.ScaleType = PreviewView.ScaleType.FIT_CENTER,
     onUseCase: (Preview?) -> Unit = { }
 ) {
     AndroidView(
-        modifier = modifier,
+        modifier = modifier.aspectRatio(3f / 4f),
         factory = { context ->
             val previewView = PreviewView(context).apply {
                 this.scaleType = scaleType
@@ -23,13 +26,20 @@ fun CameraPreview(
                     ViewGroup.LayoutParams.MATCH_PARENT
                 )
             }
-            onUseCase(
-                Preview.Builder()
-                    .build()
-                    .also {
-                        it.setSurfaceProvider(previewView.surfaceProvider)
-                    }
-            )
+            // Create a ResolutionSelector with an AspectRatioStrategy
+            val resolutionSelector = ResolutionSelector.Builder()
+                .setAspectRatioStrategy(
+                    AspectRatioStrategy.RATIO_4_3_FALLBACK_AUTO_STRATEGY
+                )
+                .build()
+
+            val preview = Preview.Builder()
+                .setResolutionSelector(resolutionSelector)
+                .build().also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+            onUseCase(preview)
             previewView
         })
 }
